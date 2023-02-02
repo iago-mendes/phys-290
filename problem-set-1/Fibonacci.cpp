8e8f9c1c2029b8e18a29106f117028342f7dba44
Iago Mendes <iagobrazmendes@gmail.com>
2023-02-02 18:40:26 -0500

    Problem set 1: question 2.

---
+++ problem-set-1/Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	printf("Number of elements: ");
+
+	int n;
+	cin >> n;
+
+	vector<long int> fibonacci_list;
+	fibonacci_list.push_back(1);
+	fibonacci_list.push_back(1);
+
+	for (int i = 0; i < n; i++) {
+		if (i > 1)
+			fibonacci_list.push_back(fibonacci_list[i-1] + fibonacci_list[i-2]);
+
+		printf("%d: %ld\n", i+1, fibonacci_list[i]);
+	}
+}
