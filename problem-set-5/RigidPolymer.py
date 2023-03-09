3c5fd14e75bb1359a14ab6e5437484f9ea3b0158
Iago Mendes <iagobrazmendes@gmail.com>
2023-03-08 23:56:02 -0500

    Problem set 5: 1b.

--- problem-set-5/RigidPolymer.py
+++ problem-set-5/RigidPolymer.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Part a
+
 nlinks = int(input('Number of links: '))
 flexibility = float(input('Flexibility: '))
 
@@ -17,7 +19,44 @@ plt.xlim(-square_half_side, square_half_side)
 plt.ylim(-square_half_side, square_half_side)
 
 plt.xlabel('x')
-plt.xlabel('y')
+plt.ylabel('y')
 plt.title(f'Polymer with {nlinks} links of flexibility {flexibility}')
+
+plt.show()
+
+# Part b
+
+print('Computing typical sizes...')
+
+nlinks = 1000
+
+def PolymerSpan(flexibility):
+	theta_relative = np.random.normal(0., flexibility, nlinks)
+	theta_absolute = np.cumsum(theta_relative)
+	x = np.cumsum(np.cos(theta_absolute))
+	y = np.cumsum(np.sin(theta_absolute))
+
+	d = np.sqrt((x[-1] - x[0])**2 + (y[-1] - y[0])**2)
+	return d
+
+def PolymerTypicalSize(flexibility):
+	sizes = np.array([])
+
+	for i in range(1000):
+		sizes = np.append(sizes, PolymerSpan(flexibility))
+	
+	return np.average(sizes)
+
+flexibilities = np.linspace(0.01, 2. * np.pi, 100)
+
+typical_sizes = np.array([])
+for flexibility in flexibilities:
+	typical_sizes = np.append(typical_sizes, PolymerTypicalSize(flexibility))
+
+plt.plot(flexibilities, typical_sizes, marker='o')
+
+plt.xlabel('Flexibility')
+plt.ylabel('Size')
+plt.title('Typical sizes for polymer with various levels of flexibility')
 
 plt.show()
